6d712d60f9ab86f8ac8d78cb0909ffafece7b0f2
Quzi4 <dontsleepletswork@gmail.com>
2023-11-20 22:20:12 +0200

    Initial commit

---
+++ Js/HW Lesson 1/main.js
@@ -0,0 +1,43 @@
+ let str1 = 'hello';
+console.log(str1)
+ let str2 = 'owu';
+console.log(str2)
+ let str3 = 'com';
+ console.log(str3)
+ let str4 = 'ua';
+ console.log(str4)
+
+let n1 = 1
+console.log(n1)
+let n2 = 10
+console.log(n2)
+let n3 = -999
+console.log(n3)
+let n4 = 123
+console.log(n4)
+let n5 = 3.14
+console.log(n5)
+let n6 = 2.7
+console.log(n6)
+let n7 = 16
+console.log(n7)
+console.log(!!25)
+console.log(!!0)
+
+let firstName = 'Vladyslav ' ;
+let middleName = 'Olegovych ' ;
+let lastName = 'Tsarenko ';
+let age = "24";
+person = firstName + middleName + lastName
+console.log(person)
+
+let a = 100
+console.log(typeof a)
+
+let b = '100'
+console.log(typeof b)
+
+let c = true
+console.log(typeof c)
+
+prompt(firstName + middleName +lastName + age)
